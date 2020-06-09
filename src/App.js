9181f6a49ecfdb22b2c96e72743d08036be457b3
fabemiliano <fab.emiliano@gmail.com>
2020-06-08 21:52:41 -0300

    fixing components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
-import StartScreen from './components/StartScreen';
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/startscreen" component={StartScreen} />
         </Switch>
       </Router>
     </div>
