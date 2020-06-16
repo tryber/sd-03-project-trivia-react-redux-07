ff4cd2d55f9e3ff7763d4f1f5e09f673ccec6b9a
Gustavo <ggteodor07@gmai.com>
2020-06-16 15:23:11 -0300

    Extra working V1.1

--- src/services/apiQuestionsService.js
+++ src/services/apiQuestionsService.js
@@ -2,20 +2,8 @@ const axios = require('axios');
 
 const URL = 'https://opentdb.com/api.php?amount=5&token=';
 
-const getQuestions = (token, confCategory, confDifficulty, confType) => {
-  console.log(token, confCategory, confDifficulty, confType);
-  return axios.get(`${URL}${token}&category=${confCategory}&difficulty=${confDifficulty}&type=${confType}`).then((response) => response)
-};
+const getQuestions = (token, confCategory, confDifficulty, confType) => (
+  axios.get(`${URL}${token}&category=${confCategory}&difficulty=${confDifficulty}&type=${confType}`).then((response) => response)
+);
 
 export default getQuestions;
-
-// async function getQuestions(token, confCategory, confDifficulty, confType) {
-//   return fetch(
-//     `https://opentdb.com/api.php?amount=5&token=${token}&category=${confCategory}&difficulty=${confDifficulty}&type=${confType}`,
-//   )
-//     .then((trivia) => trivia
-//       .json()
-//       .then((json) => (trivia.ok ? Promise.resolve(json) : Promise.reject(json))));
-// }
-
-// export default getQuestions;
